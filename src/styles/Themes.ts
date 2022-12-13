2e19adb15752fdd8e5788d7a422edce94c101123
김승훈 <rlatmdgns94@naver.com>
2022-12-13 22:21:04 +0900

    feat: 메인 레이아웃 작업

---
+++ src/styles/Themes.ts
@@ -0,0 +1,27 @@
+import { DefaultTheme } from 'styled-components'
+
+/**
+ * Color System
+ */
+// const colors = {}
+
+// const fonts = {}
+
+const fontWeights = {
+  REGULAR: 400,
+  BOLD: 700,
+}
+
+const viewSizes = {
+  MB: '@media only screen and (max-width: 1199px)',
+  PC: '@media only screen and (min-width: 1200px)',
+}
+
+const Themes: DefaultTheme = {
+  // colors,
+  // fonts,
+  fontWeights,
+  viewSizes,
+}
+
+export default Themes
