630fa6b4fc836468f0181bfdf30f35bbad02b99c
김승훈 <rlatmdgns94@naver.com>
2022-12-15 21:12:00 +0900

    feat: 공통 레이아웃 UI 작업

---
+++ src/components/layouts/MainHeader/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.header`
+  position: sticky;
+  top: 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 420px;
+  height: 50px;
+  border-bottom: 1px solid #ddd;
+`
