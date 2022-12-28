be728a5ef7675abf74ae834f238d0081fe49fa50
김승훈 <rlatmdgns94@naver.com>
2022-12-29 01:52:37 +0900

    style: 이미지 박스 영역 수정

--- src/components/molecules/Card/style.ts
+++ src/components/molecules/Card/style.ts
@@ -45,6 +45,6 @@ export const Date = styled.span`
 export const ImageBox = styled.div`
   min-width: 335px;
   width: 100%;
-  height: 340px;
+  height: 330px;
   background: ${(props) => props.theme.colors.GRAY};
 `
