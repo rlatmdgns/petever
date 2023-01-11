2d51149da83913e8ff1b4c21be23ef1532b3c1f9
김승훈 <rlatmdgns94@naver.com>
2023-01-11 23:50:30 +0900

    feat: default SEO 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { NextPage } from 'next'
 import Themes from '@/styles/Themes'
 import { ThemeProvider } from 'styled-components'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { DefaultSeo } from 'next-seo'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -13,6 +14,18 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
+}
+const DEFAULT_SEO = {
+  title: '펫에버 - 반려동물 지식 정보 커뮤니티',
+  description:
+    'You are by my side, I am by your side.\n 언제나 곁을 지킬 수 있도록 펫에버가 도와드릴게요.',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    title: '펫에버',
+    description:
+      'You are by my side, I am by your side.\n 언제나 곁을 지킬 수 있도록 펫에버가 도와드릴게요.',
+  },
 }
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
@@ -30,11 +43,14 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component?.getLayout ?? ((page) => page)
 
   return (
-    <ThemeProvider theme={Themes}>
-      <GlobalStyles />
-      <QueryClientProvider client={queryClient}>
-        <MainLayout>{getLayout(<Component {...pageProps} />)}</MainLayout>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <ThemeProvider theme={Themes}>
+        <GlobalStyles />
+        <QueryClientProvider client={queryClient}>
+          <MainLayout>{getLayout(<Component {...pageProps} />)}</MainLayout>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
   )
 }
