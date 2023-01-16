a198a4f3ca3941b0878ac2d707cfd5e34f0f8b5f
김승훈 <rlatmdgns94@naver.com>
2023-01-17 03:53:29 +0900

    feat:  Jest and React Testing Library setup

--- jest.config.js
+++ jest.config.js
@@ -10,12 +10,9 @@ const createJestConfig = nextJest({
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const customJestConfig = {
   collectCoverage: true,
-  moduleNameMapper: {
-    axios: 'axios/dist/node/axios.cjs',
-    ...pathsToModuleNameMapper(compilerOptions.paths, {
-      prefix: `<rootDir>/${compilerOptions.baseUrl}/`,
-    }),
-  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: `<rootDir>/${compilerOptions.baseUrl}/`,
+  }),
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   verbose: true,
