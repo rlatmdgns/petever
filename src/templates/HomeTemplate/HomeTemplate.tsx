d37ca10fe7d12546b59f90481633c52b3ce7a37c
김승훈 <rlatmdgns94@naver.com>
2023-01-17 03:20:42 +0900

    refactor: 네이밍 변경 -카멜케이스

--- src/templates/HomeTemplate/HomeTemplate.tsx
+++ src/templates/HomeTemplate/HomeTemplate.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import * as Styles from './styles'
-import CardList from '@/templates/Hometemplate/CardList'
+import CardList from '@/templates/HomeTemplate/CardList'
 import { Board } from '@/service/getBoards/boards.type'
 
 interface HomeTemplateProps {
   initialData: Board[]
 }
 
-const HomeTemplate = ({ initialData }: HomeTemplateProps) => {
+const HomeTemplate = ({initialData}: HomeTemplateProps) => {
   return (
     <Styles.Wrapper>
-      <CardList boards={initialData} />
+      <CardList boards={initialData}/>
     </Styles.Wrapper>
   )
 }
