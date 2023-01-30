63b2608e2c4b3a18c3bb423fbcff8fedf9828004
김승훈 <rlatmdgns94@naver.com>
2023-01-30 14:28:22 +0900

    refactor: import 오류 수정

--- src/service/getCategory/getCategory.ts
+++ src/service/getCategory/getCategory.ts
@@ -1,11 +1,10 @@
-import { apiService } from '@/core/apiClient'
+import { apiClient } from '@/core/apiClient'
 import { API_URL } from '@/core/apiUrl'
 import { Category } from '@/service/getCategory/category.type'
 
 export const getCategory = async (): Promise<Category[]> => {
   try {
-    const api = await apiService()
-    const { data } = await api.get(API_URL.GET_CATEGORY)
+    const { data } = await apiClient.get(API_URL.GET_CATEGORY)
     return data
   } catch (error) {
     throw error
