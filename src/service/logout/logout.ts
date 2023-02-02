d11723a8037b8ddfe946dd300392905d2a4a49f9
김승훈 <rlatmdgns94@naver.com>
2023-02-03 01:29:00 +0900

    fix: import 오류 수정

--- src/service/logout/logout.ts
+++ src/service/logout/logout.ts
@@ -1,10 +1,9 @@
-import { apiService } from '@/core/apiService'
 import { API_URL } from '@/core/apiUrl'
+import { apiClient } from '@/core/apiClient'
 
 export const logout = async () => {
   try {
-    const api = await apiService()
-    const { data } = await api.post(API_URL.LOGO_OUT)
+    const { data } = await apiClient.post(API_URL.LOGO_OUT)
     return data
   } catch (error) {
     throw error
